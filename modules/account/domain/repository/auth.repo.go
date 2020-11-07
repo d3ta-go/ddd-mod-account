42bf510ffc6eaea5b6ff5104e1cf470cd9a51c44
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-11-07 21:40:28 +0700

    domain.layer: add auth repository/contract

--- modules/account/domain/repository/auth.repo.go
+++ modules/account/domain/repository/auth.repo.go
@@ -1,1 +1,14 @@
 package repository
+
+import (
+	domSchemaAuth "github.com/d3ta-go/ddd-mod-account/modules/account/domain/schema/auth"
+	"github.com/d3ta-go/system/system/identity"
+)
+
+// IAuthenticationRepo represent AuthenticationRepo Interface
+type IAuthenticationRepo interface {
+	Register(req *domSchemaAuth.RegisterRequest, i identity.Identity) (*domSchemaAuth.RegisterResponse, error)
+	ActivateRegistration(req *domSchemaAuth.ActivateRegistrationRequest, i identity.Identity) (*domSchemaAuth.ActivateRegistrationResponse, error)
+	Login(req *domSchemaAuth.LoginRequest, i identity.Identity) (*domSchemaAuth.LoginResponse, error)
+	LoginApp(req *domSchemaAuth.LoginAppRequest, i identity.Identity) (*domSchemaAuth.LoginAppResponse, error)
+}
