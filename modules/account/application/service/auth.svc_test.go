c8b7da3cb2d24abef66d930d8d1a5b513b7a4f0d
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-11-14 21:40:24 +0700

    app.layer: refactor test (TDD, unit test)

--- modules/account/application/service/auth.svc_test.go
+++ modules/account/application/service/auth.svc_test.go
@@ -2,11 +2,14 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	appDTOAuth "github.com/d3ta-go/ddd-mod-account/modules/account/application/dto/auth"
 	"github.com/d3ta-go/system/system/handler"
 	"github.com/d3ta-go/system/system/initialize"
+	"github.com/d3ta-go/system/system/utils"
+	"github.com/spf13/viper"
 )
 
 func newAuthenticationSvc(t *testing.T) (*AuthenticationSvc, *handler.Handler, error) {
@@ -15,12 +18,22 @@ func newAuthenticationSvc(t *testing.T) (*AuthenticationSvc, *handler.Handler, e
 		return nil, nil, err
 	}
 
-	c, err := newConfig(t)
+	c, v, err := newConfig(t)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	h.SetDefaultConfig(c)
+	h.SetViper("config", v)
+
+	// viper for test-data
+	viperTest := viper.New()
+	viperTest.SetConfigType("yaml")
+	viperTest.SetConfigName("test-data")
+	viperTest.AddConfigPath("../../../../conf/data")
+	viperTest.ReadInConfig()
+	h.SetViper("test-data", viperTest)
+
 	if err := initialize.LoadAllDatabaseConnection(h); err != nil {
 		return nil, nil, err
 	}
@@ -40,13 +53,20 @@ func TestAuthenticationService_Register(t *testing.T) {
 		return
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.account.auth.register")
+
+	unique := utils.GenerateUUID()
 	req := appDTOAuth.RegisterReqDTO{}
-	req.Username = "admin.d3tago"
-	req.Password = "P4s$W0rd!@!"
-	req.Email = "admin.d3tago@email.com"
-	req.NickName = "Hari"
-	req.Captcha = "just-capthcha-value" // validation on interface
-	req.CaptchaID = "just-chaptcha-id"  // validation on interface
+	req.Username = fmt.Sprintf(testData["username"], unique)
+	req.Password = testData["password"]
+	req.Email = fmt.Sprintf(testData["email"], unique)
+	req.NickName = testData["nick-name"]
+	req.Captcha = testData["captcha-value"] // validation on interface
+	req.CaptchaID = testData["captcha-id"]  // validation on interface
 
 	i := newIdentity(h, t)
 
@@ -61,6 +81,18 @@ func TestAuthenticationService_Register(t *testing.T) {
 		if err != nil {
 			t.Errorf("respJSON: %s", err.Error())
 		}
+		// save to test-data
+		// save result for next test
+		viper.Set("test-data.account.auth.activate-registration.activation-code", resp.ActivationCode)
+		viper.Set("test-data.account.auth.activate-registration.email", resp.Email)
+
+		viper.Set("test-data.account.auth.login.username", req.Username)
+		viper.Set("test-data.account.auth.login.password", req.Password)
+		viper.Set("test-data.account.auth.login.captcha-value", req.Captcha)
+		viper.Set("test-data.account.auth.login.captcha-id", req.CaptchaID)
+		if err := viper.WriteConfig(); err != nil {
+			t.Errorf("Error: viper.WriteConfig(), %s", err.Error())
+		}
 		t.Logf("Resp: %s", respJSON)
 	}
 }
@@ -72,8 +104,14 @@ func TestAuthenticationService_ActivateRegistration(t *testing.T) {
 		return
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.account.auth.activate-registration")
+
 	req := appDTOAuth.ActivateRegistrationReqDTO{}
-	req.ActivationCode = "a70112cc-bca6-45c2-9bb6-cf3a56daf566"
+	req.ActivationCode = testData["activation-code"]
 
 	i := newIdentity(h, t)
 
@@ -99,11 +137,17 @@ func TestAuthenticationService_Login(t *testing.T) {
 		return
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.account.auth.login")
+
 	req := appDTOAuth.LoginReqDTO{}
-	req.Username = "admin.d3tago"
-	req.Password = "P4s$W0rd!@!"
-	req.Captcha = "just-capthcha-value" // validation on interface
-	req.CaptchaID = "just-chaptcha-id"  // validation on interface
+	req.Username = testData["username"]
+	req.Password = testData["password"]
+	req.Captcha = testData["captcha-value"] // validation on interface
+	req.CaptchaID = testData["captcha-id"]  // validation on interface
 
 	i := newIdentity(h, t)
 
@@ -118,6 +162,13 @@ func TestAuthenticationService_Login(t *testing.T) {
 		if err != nil {
 			t.Errorf("respJSON: %s", err.Error())
 		}
+		// save to test-data
+		// save result for next test
+		viper.Set("test-data.account.auth.response.session.login.token", resp.Token)
+		viper.Set("test-data.account.auth.response.session.login.expiret-at", resp.ExpiredAt)
+		if err := viper.WriteConfig(); err != nil {
+			t.Errorf("Error: viper.WriteConfig(), %s", err.Error())
+		}
 		t.Logf("Resp: %s", respJSON)
 	}
 }
@@ -129,9 +180,15 @@ func TestAuthenticationService_LoginApp(t *testing.T) {
 		return
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.account.auth.login-app")
+
 	req := appDTOAuth.LoginAppReqDTO{}
-	req.ClientKey = "53102ba5-b6b2-47ad-a68d-682463a8be29"
-	req.SecretKey = "OTk5ZDlmYjJlZGUyMjAxNTZkZThiNmNkMmJmNDI1NjdiNTYzMzcxNDEwNzNiNDBjM2NhZmIxOWY3NzZmYzhmNg=="
+	req.ClientKey = testData["client-key"]
+	req.SecretKey = testData["secret-key"]
 
 	i := newIdentity(h, t)
 
@@ -145,6 +202,15 @@ func TestAuthenticationService_LoginApp(t *testing.T) {
 		respJSON, err := json.Marshal(resp)
 		if err != nil {
 			t.Errorf("respJSON: %s", err.Error())
+		}
+		// save to test-data
+		// save result for next test
+		viper.Set("test-data.account.auth.response.session.login-app.token", resp.Token)
+		viper.Set("test-data.account.auth.response.session.login-app.expiret-at", resp.ExpiredAt)
+		viper.Set("test-data.account.auth.response.session.login-app.client-app-code", resp.ClientAppCode)
+
+		if err := viper.WriteConfig(); err != nil {
+			t.Errorf("Error: viper.WriteConfig(), %s", err.Error())
 		}
 		t.Logf("Resp: %s", respJSON)
 	}
--- modules/account/application/service/helper_test.go
+++ modules/account/application/service/helper_test.go
@@ -1,21 +1,26 @@
 package service
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/d3ta-go/system/system/config"
 	"github.com/d3ta-go/system/system/context"
 	"github.com/d3ta-go/system/system/handler"
 	"github.com/d3ta-go/system/system/identity"
+	"github.com/spf13/viper"
 )
 
-func newConfig(t *testing.T) (*config.Config, error) {
-	c, _, err := config.NewConfig("../../../../conf")
+func newConfig(t *testing.T) (*config.Config, *viper.Viper, error) {
+	c, v, err := config.NewConfig("../../../../conf")
 	if err != nil {
-		return nil, err
+		return nil, nil, err
+	}
+	if !c.CanRunTest() {
+		panic(fmt.Sprintf("Cannot Run Test on env `%s`, allowed: %v", c.Environment.Stage, c.Environment.RunTestEnvironment))
 	}
 	c.IAM.Casbin.ModelPath = "../../../../conf/casbin/casbin_rbac_rest_model.conf"
-	return c, nil
+	return c, v, nil
 }
 
 func newIdentity(h *handler.Handler, t *testing.T) identity.Identity {
